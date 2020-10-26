04a36e94e8c7cbc86d76222d5633d6fc5778e869
dmitrymakhonin <46838135+dmitrymakhonin@users.noreply.github.com>
2020-10-26 04:48:04 +0300

    extra values have been deleted from all cases

--- test/test.py
+++ test/test.py
@@ -7,7 +7,6 @@ import pytest
 @pytest.mark.parametrize("input",test_data_set1,ids=[repr(x) for x in test_data_set1])
 #Тест, который использует параметры, содержит позитивный сценарий и негативный для примера
 def test_for_check(req,input):
-    b= req.my_request(input)
     a=[[1, True, 3, 'today', 1], [2, True, 2, 'today', 2], 'Продукта с ID5 не существует', 'Продукта с ID6 не существует']
     assert req.my_request(input)==a
 # Проверка форматов полученных данных (Неудачная попытка) Проверка всех типов переменных,
--- test/test_positive.py
+++ test/test_positive.py
@@ -5,7 +5,6 @@ import pytest
 #Позитивный сценарий с повторяющимися ItemsID
 @pytest.mark.parametrize("input",test1_duplicate_meanings,ids=[repr(x) for x in test1_duplicate_meanings])
 def test_for_duplicate(req,input):
-    b= req.my_request(input)
     a=[[1, True, 3, 'today', 1], [1, True, 3, 'today', 1], [1, True, 3, 'today', 1], [1, True, 3, 'today', 1]]
     assert req.my_request(input)==a
 #все тесты будут заданы отдельной функцией, чтобы в отчете они были разделены, это поможет в их анализе
@@ -13,12 +12,8 @@ def test_for_duplicate(req,input):
 #Указано только 1 Item
 @pytest.mark.parametrize("input",only_one_item,ids=[repr(x) for x in only_one_item])
 def test_only_one_item(req,input):
-    b= req.my_request(input)
-    a=[[1, True, 3, 'today', 1]]
-    assert req.my_request(input)==a
+    assert req.my_request(input)==[[1, True, 3, 'today', 1]]
 #Указано несколько Items все валидные
 @pytest.mark.parametrize("input",several_valid_items,ids=[repr(x) for x in several_valid_items])
 def test_several_valid_items(req,input):
-    b= req.my_request(input)
-    a=[[1, True, 3, 'today', 1],[2, True, 2, 'today', 2]]
-    assert req.my_request(input)==a
+    assert req.my_request(input)==[[1, True, 3, 'today', 1],[2, True, 2, 'today', 2]]
